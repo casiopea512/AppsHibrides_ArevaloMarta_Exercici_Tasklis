f4c43a8c8039cc7079e6f4abd118541f526771b7
Marta Arevalo <martaarevaloprieto@gmail.com>
2024-11-19 19:47:21 +0100

    Funcionalidad de "Delete"

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+    
     // Inicializar el acordeón
     $("#accordion").accordion({
         collapsible: true
@@ -13,7 +14,12 @@ document.addEventListener("DOMContentLoaded", function () {
     function addTask() {
         // Crear nuevo encabezado y contenido para el acordeón
         var newHeader = $("<h3>" + taskName.val() + "</h3>");
-        var newContent = $("<div><p>" + taskDescription.val() + "</p></div>");
+        var newContent = $(`
+            <div>
+                <p>${taskDescription.val()}</p>
+                <button class="delete-task">Delete</button>
+            </div>
+        `);
 
         // Añadir el nuevo encabezado y contenido al acordeón
         $("#accordion").append(newHeader);
@@ -26,6 +32,24 @@ document.addEventListener("DOMContentLoaded", function () {
         dialog.dialog("close");
         return true;
     }
+
+    // eliminar
+    function deleteTask(event) {
+        // Prevenir acción predeterminada
+        // event.preventDefault();
+
+        // Encontrar el contenido y el encabezado relacionados
+        var contentToDelete = $(this).parent();
+        var headerToDelete = contentToDelete.prev("h3");
+
+        // Eliminar encabezado y contenido
+        headerToDelete.remove();
+        contentToDelete.remove();
+
+        // Refrescar el acordeón
+        $("#accordion").accordion("refresh");
+    }
+
 
     dialog = $("#dialog-form").dialog({
         autoOpen: false,
@@ -48,6 +72,9 @@ document.addEventListener("DOMContentLoaded", function () {
     $("#create-task").button().on("click", function () {
         dialog.dialog("open");
     });
+
+     // Delegar el evento de clic para el botón "Delete"
+    $("#accordion").on("click", ".delete-task", deleteTask);
 
     
 });
